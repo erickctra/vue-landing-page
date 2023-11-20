460ffbc2d77148abea9ceac5ec684c9ea38311d6
Erick Cintra <erickcintra@proton.me>
2023-11-19 23:14:49 -0300

    add package lazy-loading

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-      <h1 class="font-bold underline">
-        Hello world!
-      </h1>
-  </div>
-</template>
-
--- src/views/HouseView.vue
+++ src/views/HouseView.vue
@@ -1,41 +1,7 @@
-<script setup>
-import Thumbnail from "@/components/thumbnail/Thumbnail.vue";
-import Footer from "@/components/Footer.vue";
-import Skeleton from "@/components/thumbnail/Skeleton.vue";
-
-import LazyComponent from 'v-lazy-component'
-</script>
-
 <template>
   <main class="max-w-7xl p-8 pb-0 mx-auto">
-    <div class="flex flex-row items-end justify-between flex-wrap gap-y-3">
-      <div class="flex flex-col gap-y-3">
-        <sub class="text-sm">Eliana Dantas</sub>
-        <h1 class="text-3xl text-white font-medium max-w-sm">EMPREENDIMENTOS DISPONÍVEIS EM TODA SÃO PAULO:</h1>
-      </div>
-
-      <p class="max-w-sm">Encontre seu novo estilo de vida com Eliana Dantas, corretora de imóveis em toda São Paulo.
-        Eliana Dantas é uma corretora de imóveis experiente que pode ajudá-lo a encontrar a casa dos seus sonhos em São
-        Paulo.</p>
-    </div>
-    <div v-if="isLoading" class="mt-12 flex flex-row items-center justify-center flex-wrap gap-3">
-      <Skeleton/>
-      <Skeleton/>
-      <Skeleton/>
-      <Skeleton/>
-      <Skeleton/>
-    </div>
-
-    <div v-else class="mt-12 flex flex-row items-center justify-center flex-wrap gap-3">
-            <Thumbnail v-for="house in houseList" :house="house"/>
-<!--      <Skeleton v-for="house in houseList"/>-->
-<!--      <LazyLoaded v-for="house in houseList" />-->
-    </div>
-
-    <hr class="h-px mt-12 bg-neutral-800 border-0">
-
     <div class="mt-12 flex flex-row flex-wrap gap-12">
-      <div class="flex flex-col gap-y-3 flex-1">
+      <div class="flex flex-col gap-y-3 flex-1 pl-12">
         <sub class="text-sm">Eliana Dantas</sub>
         <h1 class="text-4xl text-white font-medium max-w-md">SUA PARCERIA DE CONFIANÇA NO MERCADO IMOBILIÁRIO!</h1>
         <p>CRECISP - 254502-F</p>
@@ -97,59 +63,17 @@ import LazyComponent from 'v-lazy-component'
       </div>
     </div>
 
-    <Footer/>
+    <h1 class="my-10  mx-auto text-center ">img...</h1>
+
+    <div class="min-h-[22rem] w-full bg-white rounded-md">
+      <div class="p-8">
+        <sub class="text-sm text-neutral-600">Eliana Dantas</sub>
+        <h1 class="text-4xl text-neutral-800 font-medium max-w-md">SUA PARCERIA DE CONFIANÇA NO MERCADO IMOBILIÁRIO!</h1>
+        <p class="text-neutral-600">"Nossos imóveis oferecem o conforto que você merece, independentemente do espaço que você procura, para atender a todas as suas necessidades."</p>
+      </div>
+    </div>
   </main>
 </template>
 
-<script>
-import gql from 'graphql-tag'
-
-const GET_HOUSES_THUMB = gql`
-  query Assets {
-    imoveis {
-      id
-      slug
-      metragem
-      zonaDoImovel
-      thumbnailImage
-    }
-  }
-`
-
-export default {
-  data() {
-    return {
-      isIntersected: false,
-      houseList: [],
-      isLoading: false,
-    }
-  },
-  created() {
-    const vm = this
-    vm.getHousesList({character: '@'})
-  },
-  methods: {
-    getHousesList(variables = {}) {
-
-      const vm = this
-      vm.isLoading = true
-
-      vm.$apolloProvider.defaultClient
-          .query({
-            query: GET_HOUSES_THUMB,
-            variables: {
-              ...variables
-            }
-          })
-          .then(result => {
-            vm.houseList = result.data.imoveis;
-            vm.isLoading = result.data.loading;
-          })
-          .catch(error => {
-            console.log(error)
-          })
-    }
-  }
-}
+<script setup>
 </script>
-
