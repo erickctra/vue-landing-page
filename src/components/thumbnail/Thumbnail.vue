ef6123f3f1634abeed879d62fc2f245ddc136270
Erick Cintra <erickcintra@proton.me>
2023-11-22 01:54:01 -0300

    add a link to home thumbnails

--- src/components/thumbnail/Thumbnail.vue
+++ src/components/thumbnail/Thumbnail.vue
@@ -17,26 +17,29 @@ defineProps({
 
 <template>
   <lazy-component class="flex flex-1 max-w-sm basis-[18rem]" wrapper-tag="section" @intersected="Intersect">
-    <div class="flex flex-col flex-1 gap-2 ">
-      <div
-          class=" w-full h-48 bg-gray-600 bg-no-repeat bg-cover bg-center rounded-lg shadow-md overflow-hidden">
-        <!--      @load event does not work with img-->
-        <img class="w-full h-48 object-cover" v-show="isLoaded" decoding="async"
-             :src="house.thumbnailImage"  alt="...">
-      </div>
-      <div class="flex flex-row gap-3 text-gray-200">
-        <p>{{ house.slug }}</p>
-        <span
-            class="inline-flex items-center rounded-md bg-neutral-400 bg-opacity-10 px-2 py-1 text-xs font-medium text-gray-200 ring-1 ring-inset ring-gray-500/10">{{
-            house.zonaDoImovel
-          }}</span>
-      </div>
-    </div>
+      <router-link :to="'/casa/' + house.slug">
 
-    <!-- Optional -->
-    <template #placeholder>
-      <Skeleton/>
-    </template>
+      <div class="flex flex-col flex-1 gap-2 ">
+        <div
+            class=" w-full h-48 bg-gray-600 bg-no-repeat bg-cover bg-center rounded-lg shadow-md overflow-hidden">
+          <!--      @load event does not work with img-->
+          <img class="w-full h-48 object-cover" v-show="isLoaded" decoding="async"
+               :src="house.thumbnailImage"  alt="...">
+        </div>
+        <div class="flex flex-row gap-3 text-gray-200">
+          <p>{{ house.slug }}</p>
+          <span
+              class="inline-flex items-center rounded-md bg-neutral-400 bg-opacity-10 px-2 py-1 text-xs font-medium text-gray-200 ring-1 ring-inset ring-gray-500/10">{{
+              house.zonaDoImovel
+            }}</span>
+        </div>
+      </div>
+
+      <!-- Optional -->
+      <template #placeholder>
+        <Skeleton/>
+      </template>
+      </router-link>
   </lazy-component>
 </template>
 
